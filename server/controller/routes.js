2dc50be1c36a4fd533b91e8637e6e52fcb1e0228
jazo84 <31776267+jazo84@users.noreply.github.com>
2017-12-10 13:55:48 -0500

    updates

--- server/controller/routes.js
+++ server/controller/routes.js
@@ -25,4 +25,35 @@ router.get('/login', function (req, res){
 	res.sendFile(path.join(__dirname,'../../client/public/html/login.html'));
 });
 
+router.post('/api/sign_up',(req,res) => {
+	console.log(req.body);
+	if(req.body.name !== "" && req.body.username !== "" && req.body.password !== "" && req.body.email !=="") {
+		var query = "INSERT INTO users (name, username, password, email) VALUES ($1, $2, $3, $4)";
+		pgClient.query(query, [req.body.name, req.body.username, req.body.password, req.body.email], (error, signUpRes)  => {
+			if (error){
+				res.json({error:error})
+			} else {
+				res.json({results:signUpRes})
+			}
+		});
+	} else if (req.body.name === "" || req.body.username === "" || req.body.password === "" || req.body.email === "") {
+		res.json({results:"a_null_field"})
+	};
+});
+
+router.post('api/login', function (req,res){
+	var query = `SELECT * FROM users WHERE username='${req.body.username}'`;
+	pgClient.query(query, (error, loginRes)=>{
+		if(req.body.password === loginRes.rows[0].password){
+			if(error){
+				res.json({error:error})
+			} else {
+				res.json({results:loginRes.rows})
+			}
+		}else {
+			res.json({error:"Incorrect Password"})
+		}
+	});
+});
+
 module.exports = router;
