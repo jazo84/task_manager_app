2313c08a8f0ffaea39e4ff00e3be9ded1f155180
jazo84 <31776267+jazo84@users.noreply.github.com>
2017-12-17 10:43:14 -0500

    project updates

--- server/controller/routes.js
+++ server/controller/routes.js
@@ -14,7 +14,7 @@ var dbURL = {
 var pgClient = new pg.Client(dbURL);
 pgClient.connect();
 
-var signed_in_html = require ('../helpers/signed_in.js')
+var signed_in_html = require ('../helpers/signed_in.js');
 var router = express.Router();
 
 router.get('/', function (req,res){
@@ -58,9 +58,8 @@ router.post('/api/login', (req,res)=>{
 });
 
 router.get('/justdoittasks/profile/:id', (req,res) =>{
-	var query = `SELECT users.name,tasks.start_date, tasks.end_date, tasks.priority, tasks.task_name, tasks.task_description, tasks.assigned_to, tasks.username FROM tasks INNER JOIN users ON tasks.username=users.username WHERE users.id=${req.params.id}`;
+	var query = `SELECT users.id, users.name, tasks.start_date, tasks.end_date, tasks.priority, tasks.task_name, tasks.task_description, tasks.assigned_to, tasks.user_id FROM tasks INNER JOIN users ON tasks.user_id=users.id WHERE tasks.user_id=${req.params.id}`;
 	pgClient.query(query, (error, userRes)=>{
-		//console.log(userRes)
 		if (error) {
 			res.json({error:error})
 		} else {
@@ -71,16 +70,18 @@ router.get('/justdoittasks/profile/:id', (req,res) =>{
 });
 
 router.post('/api/task', (req, res) =>{
-	if(req.body.task_name !== '' && req.body.task_description !== '')
-		var query = "INSERT INTO tasks (task_name, task_description, start_date, end_date, assigned_to, priority) VALUES ($1, $2, $3, $4, $5, $6)";
-	pgClient.query(query, [req.body.task_name, req.body.task_description, req.body.start_date, req.body.end_date, req.body.assigned_to, req.body.priority], (error, taskres) =>{
-		console.log(req.body)
+	if(req.body.task_name !== '' && req.body.task_description !== '') {
+		var query = "INSERT INTO tasks (task_name, task_description, start_date, end_date, assigned_to, priority, user_id) VALUES ($1, $2, $3, $4, $5, $6, $7)";
+	pgClient.query(query, [req.body.task_name, req.body.task_description, req.body.start_date, req.body.end_date, req.body.assigned_to, req.body.priority, req.body.user_id], (error, taskRes) => {
 		if (error) {
 			res.json({error:error})
 		} else {
-			res.json({results:taskres.rows})
+			res.json({results:taskRes})
 		}
 	});
+	} else if (req.body.task_name === "" || req.body.task_description === "") {
+		res.json ({results:"Missing_Task_Info"})
+	}
 });
 
 module.exports = router;
